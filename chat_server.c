803464dca046e4b6eeb64ea313e1a78bae247caf
brondum03 <brandonauyongth@gmail.com>
2025-11-24 16:00:31 +0000

    return incoming client ip address

--- chat_server.c
+++ chat_server.c
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
     while (1) 
     {
         // Storage for request and response messages
-        // Added storage for client's port
-        char client_request[BUFFER_SIZE], server_response[BUFFER_SIZE], client_port[6];
+        // Added storage for client's port (xxxxx\0) and IP address (max ip addr length)
+        char client_request[BUFFER_SIZE], server_response[BUFFER_SIZE], client_port[6], client_ip[INET_ADDRSTRLEN];
 
         // Demo code (remove later)
         printf("Server is listening on port %d\n", SERVER_PORT);
@@ -36,11 +36,14 @@ int main(int argc, char *argv[])
         {
             // Demo code (remove later)
             int client_port_int = ntohs(client_address.sin_port);
+            const char* client_ip_addr = inet_ntop(AF_INET, &client_address.sin_addr, client_ip, INET_ADDRSTRLEN);
             snprintf(client_port, sizeof(client_port), "%d", client_port_int);
             strcpy(server_response, "Hi, the server has received: ");
             strcat(server_response, client_request);
             strcat(server_response, " from port ");
             strcat(server_response, client_port);
+            strcat(server_response, " from IP address ");
+            strcat(server_response, client_ip_addr);
             strcat(server_response, "\n");
 
             // This function writes back to the incoming client,
